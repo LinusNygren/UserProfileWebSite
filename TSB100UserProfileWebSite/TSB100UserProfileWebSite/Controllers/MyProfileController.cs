96cb525aaf4ddbcb67c24e40adb9fd6f38d1faa3
LinusNygren <50328460+LinusNygren@users.noreply.github.com>
2019-05-14 11:21:41 +0200

    Add files via upload

---
+++ TSB100UserProfileWebSite/TSB100UserProfileWebSite/Controllers/MyProfileController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSB100UserProfileWebSite.Controllers
+{
+    public class MyProfileController : Controller
+    {
+        // GET: MyProfile
+        public ActionResult Index()
+        {
+            ProfileServiceReference.UserProfileServiceClient client = new ProfileServiceReference.UserProfileServiceClient();
+            return View();
+        }
+    }
+}
